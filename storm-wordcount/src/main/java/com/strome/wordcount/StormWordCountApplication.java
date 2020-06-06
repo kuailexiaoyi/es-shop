77835dd6168122d0602772dc713d5e26fbc36523
kuailexiaoyi <409718341@qq.com>
2020-06-06 20:07:04 +0800

    1.【storm-wordcount】使用storm技术实时统计字串数量。

--- storm-wordcount/src/main/java/com/strome/wordcount/StormWordCountApplication.java
+++ storm-wordcount/src/main/java/com/strome/wordcount/StormWordCountApplication.java
@@ -1,34 +1,7 @@
 package com.strome.wordcount;
 
-import com.strome.wordcount.bolt.SplitSentenceBolt;
-import com.strome.wordcount.bolt.WordCountBolt;
-import com.strome.wordcount.spout.RandomSentenceSpout;
-import com.strome.wordcount.utils.StormUtil;
-import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
-import org.apache.storm.StormSubmitter;
-import org.apache.storm.topology.TopologyBuilder;
-import org.apache.storm.tuple.Fields;
-
 public class StormWordCountApplication {
     public static void main(String[] args) throws Exception {
-        TopologyBuilder topologyBuilder = new TopologyBuilder();
-        topologyBuilder.setSpout("spout", new RandomSentenceSpout(), 1);
-        topologyBuilder.setBolt("splitbolt", new SplitSentenceBolt(), 1).shuffleGrouping("spoput");
-        topologyBuilder.setBolt("count", new WordCountBolt(), 1).fieldsGrouping("splitbolt", new Fields("word"));
 
-        Config config = new Config();
-        config.setDebug(true);
-
-        if (args != null && args.length > 0) {
-            config.setNumWorkers(3);
-            StormSubmitter.submitTopologyWithProgressBar(args[0], config, topologyBuilder.createTopology());
-        } else {
-            config.setMaxTaskParallelism(3);
-            LocalCluster localCluster = new LocalCluster();
-            localCluster.submitTopology("word-count", config, topologyBuilder.createTopology());
-            StormUtil.sleep(1000);
-            localCluster.shutdown();
-        }
     }
 }
--- storm-wordcount/src/main/java/com/strome/wordcount/topology/WordCountTopology.java
+++ storm-wordcount/src/main/java/com/strome/wordcount/topology/WordCountTopology.java
@@ -1,5 +1,15 @@
 package com.strome.wordcount.topology;
 
+import com.strome.wordcount.bolt.SplitSentenceBolt;
+import com.strome.wordcount.bolt.WordCountBolt;
+import com.strome.wordcount.spout.RandomSentenceSpout;
+import com.strome.wordcount.utils.StormUtil;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+
 /**
  * @Desc: 字串统计 拓扑
  * @Date: 2020-06-06
@@ -7,7 +17,24 @@ package com.strome.wordcount.topology;
  */
 public class WordCountTopology {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        TopologyBuilder topologyBuilder = new TopologyBuilder();
+        topologyBuilder.setSpout("spout", new RandomSentenceSpout(), 1);
+        topologyBuilder.setBolt("splitbolt", new SplitSentenceBolt(), 1).shuffleGrouping("spoput");
+        topologyBuilder.setBolt("count", new WordCountBolt(), 1).fieldsGrouping("splitbolt", new Fields("word"));
 
+        Config config = new Config();
+        config.setDebug(true);
+
+        if (args != null && args.length > 0) {
+            config.setNumWorkers(3);
+            StormSubmitter.submitTopologyWithProgressBar(args[0], config, topologyBuilder.createTopology());
+        } else {
+            config.setMaxTaskParallelism(3);
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("word-count", config, topologyBuilder.createTopology());
+            StormUtil.sleep(1000);
+            localCluster.shutdown();
+        }
     }
 }
